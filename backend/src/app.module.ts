31396de3170355dfc557e7c8ee182fd1f97b69e0
krims31 <vegor405@gmail.com>
2026-09-04 19:50:15 +0300

    добавил через nest cli все файлы в папки auth users tasks projects

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule, AuthModule, ProjectsModule, TasksModule],
   controllers: [],
   providers: [],
 })
