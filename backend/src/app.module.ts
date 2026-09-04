2ab512e67192a06565360abe74961a8a5d3eab1c
krims31 <vegor405@gmail.com>
2026-09-04 19:56:55 +0300

    небольшое изменение просто добавил ;

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { ProjectsModule } from './projects/projects.module';
 import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [PrismaModule, UsersModule, AuthModule, ProjectsModule, TasksModule],
