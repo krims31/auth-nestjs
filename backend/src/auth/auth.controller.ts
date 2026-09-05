ee84d23adc2157b3912d4a38b57fd929399b5f0b
krims31 <vegor405@gmail.com>
2026-09-05 19:49:33 +0300

    создал новый метод register

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateDto } from '../dto/CreateDto.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() dto: CreateDto) {
+    return await this.authService.register(dto);
+  }
+}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { CreateDto } from '../dto/CreateDto.dto';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async register(dto: CreateDto) {
+    return await this.usersService.create(dto);
+  }
+}
