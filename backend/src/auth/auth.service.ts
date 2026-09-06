de1770b702b1742bf8bf46418b4037feb988079b
krims31 <vegor405@gmail.com>
2026-09-06 12:01:40 +0300

    создал метод login, импортировал и задал значения в auth.module.ts для JWT задал секретный ключ и время длительности токена

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,12 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 import { CreateDto } from '../dto/CreateDto.dto';
+import { LoginDto } from '../dto/LoginDto.dto';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async register(dto: CreateDto) {
     return await this.usersService.create(dto);
+  }
+
+  async login(dto: LoginDto) {
+    const user = await this.usersService.findByEmail(dto.email);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPassword = await bcrypt.compare(dto.password, user.passwordHash);
+
+    if (!isPassword) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return {
+      access_token: this.jwtService.sign({ sub: user.id, email: user.email }),
+    };
   }
 }
