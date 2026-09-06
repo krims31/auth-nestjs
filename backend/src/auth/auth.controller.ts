ff0895bd5e8c73042df7e9022776d9fd593d7071
krims31 <vegor405@gmail.com>
2026-09-06 12:05:43 +0300

    добавил метод login в auth.controller.ts

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateDto } from '../dto/CreateDto.dto';
+import { LoginDto } from '../dto/LoginDto.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('register')
   async register(@Body() dto: CreateDto) {
     return await this.authService.register(dto);
+  }
+
+  @Post('login')
+  async login(@Body() dto: LoginDto) {
+    return await this.authService.login(dto);
   }
 }
