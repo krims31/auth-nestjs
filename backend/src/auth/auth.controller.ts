41153b534144ead6fc5f7a6ff4120d0fe540c99f
krims31 <vegor405@gmail.com>
2026-09-07 13:10:46 +0300

    добавил метод getMe с декораторами useGuards и getMe для запроса и проверки ошибки 401 Unauthorized

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,11 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
 import { CreateDto } from '../dto/CreateDto.dto';
 import { LoginDto } from '../dto/LoginDto.dto';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt.auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getMe(@Req() req: Request) {
+    return req.user;
+  }
 
   @Post('register')
   async register(@Body() dto: CreateDto) {
