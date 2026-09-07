eb6ad9e5e46156318aa9999f770ad9833b046640
krims31 <vegor405@gmail.com>
2026-09-07 17:54:34 +0300

    создал rolesGuard и описал логику для RBAC

---
+++ backend/src/auth/RolesGuard.ts
@@ -0,0 +1,27 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+
+    if (!roles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const user = request.user;
+
+    if (!user) {
+      return false;
+    }
+    return roles.includes(user.role);
+  }
+}
