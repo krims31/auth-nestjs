6e91a990c336b90f108cabaf3711f0f75f4a24d3
krims31 <vegor405@gmail.com>
2026-09-10 08:56:54 +0300

    создал create метод и передал туда title description ownerId

--- backend/src/projects/projects.service.ts
+++ backend/src/projects/projects.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CreateProjectDto } from '../dto/CreateProject.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(dto: CreateProjectDto, ownerId: string) {
+    return await this.prisma.project.create({
+      data: {
+        title: dto.title,
+        description: dto.description,
+        ownerId,
+      },
+    });
+  }
+}
