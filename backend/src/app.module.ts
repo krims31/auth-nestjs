d2b36d21d521a6c27995586986c4effb19952527
krims31 <vegor405@gmail.com>
2026-09-10 08:45:40 +0300

    переместил папку tasks внутрь папки projects и создал так же dto файл для Project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProjectsModule } from './projects/projects.module';
-import { TasksModule } from './tasks/tasks.module';
+import { TasksModule } from './projects/tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
---
+++ backend/src/dto/CreateProject.dto.ts
@@ -0,0 +1,9 @@
+import { IsOptional, IsString } from 'class-validator';
+
+export class CreateProjectDto {
+  @IsString()
+  title: string;
+  @IsString()
+  @IsOptional()
+  description: string;
+}
