b52481d8d00b383e027d169b6ca7454375db9e6d
krims31 <vegor405@gmail.com>
2026-09-11 12:53:34 +0300

    добавил методы create, findAll, findOneProject, Patch, Delete

--- backend/src/projects/projects.controller.ts
+++ backend/src/projects/projects.controller.ts
@@ -1,4 +1,62 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt.auth.guard';
+import { CreateProjectDto } from '../dto/CreateProject.dto';
+import { UpdateProjectDto } from '../dto/UpdateProjectDto.dto';
+import { CurrentUser } from './../common/decorators/currentUser.decorator';
+import { ProjectsService } from './projects.service';
 
+@UseGuards(JwtAuthGuard)
 @Controller('projects')
-export class ProjectsController {}
+export class ProjectsController {
+  constructor(private readonly projectsService: ProjectsService) {}
+
+  @Post()
+  async create(
+    @Body() dto: CreateProjectDto,
+    @CurrentUser() user: Express.User,
+  ) {
+    return await this.projectsService.create(dto, user.id);
+  }
+
+  @Get()
+  async findAll(@CurrentUser() user: Express.User) {
+    return await this.projectsService.findAllByUser(user.id);
+  }
+
+  @Get(':id')
+  async findOneProject(
+    @Param('id') id: string,
+    @CurrentUser() user: Express.User,
+  ) {
+    return await this.projectsService.findOne(id, user.id);
+  }
+
+  @Patch(':id')
+  async updateProject(
+    @Param('id')
+    id: string,
+    @CurrentUser() user: Express.User,
+    @Body()
+    dto: UpdateProjectDto,
+  ) {
+    return await this.projectsService.update(id, user.id, dto);
+  }
+
+  @Delete(':id')
+  async delete(
+    @Param('id')
+    id: string,
+    @CurrentUser() user: Express.User,
+  ) {
+    return await this.projectsService.delete(id, user.id);
+  }
+}
