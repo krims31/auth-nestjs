138f3ece748a196152659b52156c10d5ae77ef4c
krims31 <vegor405@gmail.com>
2026-09-12 10:19:05 +0300

    создал методы create, findAllByProject, findOne, update, delete

--- backend/src/projects/tasks/tasks.service.ts
+++ backend/src/projects/tasks/tasks.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTaskDto } from '../../dto/CreateTaskDto.dto';
+import { UpdateTaskDto } from '../../dto/UpdateTaskDto.dto';
+import { PrismaService } from '../../prisma/prisma.service';
+import { ProjectsService } from '../projects.service';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly projectsService: ProjectsService,
+  ) {}
+
+  async create(dto: CreateTaskDto, projectId: string, ownerId: string) {
+    await this.projectsService.findOne(projectId, ownerId);
+    return await this.prisma.task.create({
+      data: {
+        title: dto.title,
+        projectId,
+      },
+    });
+  }
+
+  async findAllByProject(projectId: string, ownerId: string) {
+    await this.projectsService.findOne(projectId, ownerId);
+    return await this.prisma.task.findMany({ where: { projectId } });
+  }
+
+  async findOne(id: string, projectId: string, ownerId: string) {
+    await this.projectsService.findOne(projectId, ownerId);
+    const task = await this.prisma.task.findFirst({
+      where: { id, projectId },
+    });
+
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+    return task;
+  }
+
+  async update(
+    id: string,
+    projectId: string,
+    ownerId: string,
+    dto: UpdateTaskDto,
+  ) {
+    await this.findOne(id, projectId, ownerId);
+
+    return await this.prisma.task.update({
+      where: { id },
+      data: dto,
+    });
+  }
+
+  async delete(id: string, projectId: string, ownerId: string) {
+    await this.findOne(id, projectId, ownerId);
+
+    return await this.prisma.task.delete({
+      where: { id },
+    });
+  }
+}
