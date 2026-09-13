eb2877c5e34d9f1ed025776dfed3fa61b103f79f
krims31 <vegor405@gmail.com>
2026-09-13 13:07:02 +0300

    исправил и добавил status метод и сделать его опциональным, так же добавил Enum

--- backend/src/dto/UpdateTaskDto.dto.ts
+++ backend/src/dto/UpdateTaskDto.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsEnum, IsOptional } from 'class-validator';
 import { CreateTaskDto } from './CreateTaskDto.dto';
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+  @IsOptional()
+  @IsEnum(['TODO', 'IN_PROGRESS', 'DONE'])
+  status?: string;
+}
