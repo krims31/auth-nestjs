33eebff9b3480243f85804ee4265e38346a754a6
krims31 <vegor405@gmail.com>
2026-09-13 12:16:08 +0300

    передал ProjectsService в providers

--- backend/src/projects/tasks/tasks.module.ts
+++ backend/src/projects/tasks/tasks.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ProjectsService } from '../projects.service';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
   controllers: [TasksController],
-  providers: [TasksService]
+  providers: [TasksService, ProjectsService],
 })
 export class TasksModule {}
