98cfbf5acb857905524d63eb9fc9350b9bebc60d
krims31 <vegor405@gmail.com>
2026-09-14 12:43:39 +0300

    перекинул через декомпозицию значения кастомного хука

--- frontend/features/auth-by-email/ui/LoginForm.tsx
+++ frontend/features/auth-by-email/ui/LoginForm.tsx
@@ -1,26 +1,7 @@
-import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { useAuthLogin } from '../model/useAuthLogin'
 
 export default function LoginForm() {
-	const router = useRouter()
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
-
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
-
-		const isSuccess = await fetch('/api/auth/login', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ email, password })
-		}).then(res => res.json())
-
-		if (isSuccess) {
-			router.push('/dashboard')
-		}
-	}
+	const { email, setEmail, password, setPassword, handleSubmit } = useAuthLogin()
 	return (
 		<>
 			<form></form>
