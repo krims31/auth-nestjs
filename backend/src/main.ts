c2ee0325f04f6fddab41202c7400dcbd8e04866a
krims31 <vegor405@gmail.com>
2026-09-14 12:07:13 +0300

    изменил порт на 3001(фронтенд часть)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:3001',
     credentials: true,
   });
 }
