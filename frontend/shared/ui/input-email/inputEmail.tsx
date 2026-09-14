83ce6ad5b2c5781971c2f6dbe20c898f238874a5
krims31 <vegor405@gmail.com>
2026-09-14 13:25:52 +0300

    сделал стилизацию

--- frontend/shared/ui/input-email/inputEmail.tsx
+++ frontend/shared/ui/input-email/inputEmail.tsx
@@ -4,11 +4,15 @@ export default function InputEmail() {
 	const { email, setEmail } = useAuthLogin()
 	return (
 		<>
-			<input
-				type="email"
-				value={email}
-				onChange={e => setEmail(e.target.value)}
-			/>
+			<div className="flex items-center justify-center h-150">
+				<input
+					type="email"
+					value={email}
+					placeholder="Email"
+					onChange={e => setEmail(e.target.value)}
+					className="border rounded-[5px] w-70 h-10 placeholder:p-10 placeholder:text-gray-500 bg-gray-200"
+				/>
+			</div>
 		</>
 	)
 }
