968adcf485a254262eb973160025fc37737f6be2
krims31 <vegor405@gmail.com>
2026-09-14 12:38:57 +0300

    добавил LoginForm

--- frontend/features/auth-by-email/ui/LoginForm.tsx
+++ frontend/features/auth-by-email/ui/LoginForm.tsx
@@ -1,7 +1,29 @@
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+
 export default function LoginForm() {
+	const router = useRouter()
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		const isSuccess = await fetch('/api/auth/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ email, password })
+		}).then(res => res.json())
+
+		if (isSuccess) {
+			router.push('/dashboard')
+		}
+	}
 	return (
 		<>
-			<h1>LoginForm</h1>
+			<form></form>
 		</>
 	)
 }
