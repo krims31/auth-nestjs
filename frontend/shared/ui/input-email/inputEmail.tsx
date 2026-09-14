e3fdaa43528367ee36dd2f47e5d8cf22f774e796
krims31 <vegor405@gmail.com>
2026-09-14 13:05:28 +0300

    добавил use strict

--- frontend/shared/ui/input-email/inputEmail.tsx
+++ frontend/shared/ui/input-email/inputEmail.tsx
@@ -1,7 +1,14 @@
+import { useAuthLogin } from '@/features/auth-by-email/model/useAuthLogin'
+
 export default function InputEmail() {
+	const { email, setEmail } = useAuthLogin()
 	return (
 		<>
-			<input type="email" />
+			<input
+				type="email"
+				value={email}
+				onChange={e => setEmail(e.target.value)}
+			/>
 		</>
 	)
 }
