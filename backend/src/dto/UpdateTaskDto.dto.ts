a34bba4940f77f7ca0fa9bb3e08ea9a3a2f51280
krims31 <vegor405@gmail.com>
2026-09-14 10:29:33 +0300

    передал TaskStatus для status и декоратора IsEnum()

--- backend/src/dto/UpdateTaskDto.dto.ts
+++ backend/src/dto/UpdateTaskDto.dto.ts
@@ -1,9 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { IsEnum, IsOptional } from 'class-validator';
+import { TaskStatus } from '../../generated/prisma/enums';
 import { CreateTaskDto } from './CreateTaskDto.dto';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
   @IsOptional()
-  @IsEnum(['TODO', 'IN_PROGRESS', 'DONE'])
-  status?: string;
+  @IsEnum(TaskStatus)
+  status?: TaskStatus;
 }
