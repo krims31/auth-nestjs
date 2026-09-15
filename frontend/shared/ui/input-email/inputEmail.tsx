49bccf2bd29c6d444eab65425d7f79723cddc1e5
krims31 <vegor405@gmail.com>
2026-09-15 10:37:00 +0300

    установил input Field компоненты

--- frontend/shared/ui/input-email/inputEmail.tsx
+++ frontend/shared/ui/input-email/inputEmail.tsx
@@ -1,18 +1,19 @@
+import { Field, FieldDescription, FieldLabel } from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
 import { useAuthLogin } from '@/features/auth-by-email/model/useAuthLogin'
 
 export default function InputEmail() {
 	const { email, setEmail } = useAuthLogin()
 	return (
-		<>
-			<div className="flex items-center justify-center h-150">
-				<input
-					type="email"
-					value={email}
-					placeholder="Email"
-					onChange={e => setEmail(e.target.value)}
-					className="border rounded-[5px] w-70 h-10 placeholder:p-10 placeholder:text-gray-500 bg-gray-200"
-				/>
-			</div>
-		</>
+		<Field data-disabled>
+			<FieldLabel htmlFor="input-demo-disabled">Email</FieldLabel>
+			<Input
+				id="input-demo-disabled"
+				type="email"
+				placeholder="Email"
+				disabled
+			/>
+			<FieldDescription>This field is currently disabled.</FieldDescription>
+		</Field>
 	)
 }
