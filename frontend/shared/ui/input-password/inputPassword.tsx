ecf6d24c76af4e361879f8eb3d1ae0bbd715c602
krims31 <vegor405@gmail.com>
2026-09-15 11:58:16 +0300

    создал папку для input-password

---
+++ frontend/shared/ui/input-password/inputPassword.tsx
@@ -0,0 +1,24 @@
+import { Field, FieldLabel } from '../../../components/ui/field'
+import { Input } from '../../../components/ui/input'
+import { useAuthLogin } from '../../../features/auth-by-email/model/useAuthLogin'
+
+export default function InputPassword() {
+	const { email, setEmail } = useAuthLogin()
+	return (
+		<Field>
+			<FieldLabel
+				htmlFor="email"
+				className="relative top-40 left-10 text-[15px] font-mono"
+			>
+				Email
+			</FieldLabel>
+			<Input
+				value={email}
+				onChange={e => setEmail(e.target.value)}
+				type="email"
+				placeholder="Email"
+				className="relative top-40 left-10 font-mono"
+			/>
+		</Field>
+	)
+}
