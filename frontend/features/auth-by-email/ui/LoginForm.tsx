f06b5d942527bc551b2f889212614001da7bdf47
krims31 <vegor405@gmail.com>
2026-09-15 11:59:26 +0300

    добавил семантический тег section и внутри него заголовок Sign In и так же передал handleSubmit в form, в form импортировал InputPassword

--- frontend/features/auth-by-email/ui/LoginForm.tsx
+++ frontend/features/auth-by-email/ui/LoginForm.tsx
@@ -1,16 +1,23 @@
 'use client'
 
 import InputEmail from '../../../shared/ui/input-email/inputEmail'
+import InputPassword from '../../../shared/ui/input-password/inputPassword'
 import { useAuthLogin } from '../model/useAuthLogin'
 
 export default function LoginForm() {
-	const { password, setPassword, handleSubmit } = useAuthLogin()
+	const { handleSubmit } = useAuthLogin()
 	return (
 		<>
 			<div className="flex items-center justify-center h-200">
 				<div className="border rounded-2xl w-100 h-120">
-					<form>
-						<InputEmail  />
+					<section>
+						<h1 className="text-center relative top-10 text-2xl font-mono">
+							Sign In
+						</h1>
+					</section>
+					<form onSubmit={handleSubmit}>
+						<InputEmail />
+						<InputPassword />
 					</form>
 				</div>
 			</div>
