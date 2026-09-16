56bd48ef5229aea674599f7c176dc8976c014d64
krims31 <vegor405@gmail.com>
2026-09-16 14:42:53 +0300

    изменил login на Create an account

---
+++ frontend/components/ui/buttonRegister.tsx
@@ -0,0 +1,12 @@
+export default function ButtonRegister() {
+	return (
+		<>
+			<button
+				type="submit"
+				className="border rounded-[10px] w-80 h-12 flex justify-center items-center relative top-32 left-11 text-[15px] font-mono bg-black text-white hover:bg-blue-600 transform duration-300 ease-in-out"
+			>
+				Create an account
+			</button>
+		</>
+	)
+}
