abf0806b7118499b7c9d86d746b9cd7f8ac70247
krims31 <vegor405@gmail.com>
2026-09-17 14:52:42 +0300

    add a new folder type and add file RegisterFromValues.ts and then a change to file useAuthRegister.ts

---
+++ frontend/features/auth-by-email/model/type/RegisterFromValues.ts
@@ -0,0 +1,7 @@
+export type RegisterFromValues = {
+	username: string
+	email: string
+	password: string
+	access_token: string
+	refresh_token: string
+}
--- frontend/features/auth-by-email/model/useAuthRegister.ts
+++ frontend/features/auth-by-email/model/useAuthRegister.ts
@@ -1,30 +1,28 @@
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { RegisterFromValues } from './type/RegisterFromValues'
 
 export default function useAuthRegister() {
 	const router = useRouter()
-	const [email, setEmail] = useState<string>('')
-	const [password, setPassword] = useState<string>('')
-	const [username, setUsername] = useState<string>('')
-	const [visible, setVisible] = useState<boolean>(false)
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<RegisterFromValues>()
 
-	const handleSubmitRegister = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
-
+	const onSubmit = async (data: RegisterFromValues) => {
 		const response = await fetch('http://localhost:3000/auth/register', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
 			},
-			body: JSON.stringify({ email, password })
+			body: JSON.stringify(data)
 		})
 
 		if (!response.ok) {
-			console.log("Register failed")
-			return;
+			console.log('Register failed')
+			return
 		}
-
-		const data = await response.json()
 
 		localStorage.setItem('access_token', data.access_token)
 		localStorage.setItem('refresh_token', data.refresh_token)
@@ -32,15 +30,5 @@ export default function useAuthRegister() {
 		router.push('/auth/login')
 	}
 
-	return {
-		email,
-		setEmail,
-		password,
-		setPassword,
-		username,
-		setUsername,
-		visible,
-		setVisible,
-		handleSubmitRegister
-	}
+	return { register, handleSubmit: handleSubmit(onSubmit), errors }
 }
