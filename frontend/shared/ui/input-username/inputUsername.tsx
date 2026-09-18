6fed6b5cf91951d427b0cc277423927befe02f8a
krims31 <vegor405@gmail.com>
2026-09-18 11:43:17 +0300

    Change a wrapper style.

--- frontend/shared/ui/input-username/inputUsername.tsx
+++ frontend/shared/ui/input-username/inputUsername.tsx
@@ -1,27 +1,38 @@
+import { AlertCircle } from 'lucide-react'
+import { UseFormRegisterReturn } from 'react-hook-form'
 import { Field, FieldLabel } from '../../../components/ui/field'
 import { Input } from '../../../components/ui/input'
-import { UseFormRegisterReturn } from 'react-hook-form'
 
 export default function InputUsername({
-	registration
+	registration,
+	error
 }: {
 	registration: UseFormRegisterReturn
+	error?: string
 }) {
 	return (
-		<Field>
-			<FieldLabel
-				htmlFor="username"
-				className="relative top-20 left-10 text-[15px] font-mono"
-			>
-				Username
-			</FieldLabel>
-			<Input
-				id="username"
-				type="text"
-				placeholder="Username"
-				{...registration}
-				className="relative top-20 left-10 font-mono w-85!"
-			/>
-		</Field>
+		<div className="flex flex-col gap-1 mb-4">
+			<Field>
+				<FieldLabel
+					htmlFor="username"
+					className="relative top-20 left-10 text-[15px] font-mono"
+				>
+					Username
+				</FieldLabel>
+				<Input
+					id="username"
+					type="text"
+					placeholder="Username"
+					{...registration}
+					className="relative top-20 left-10 font-mono w-85!"
+				/>
+				{error && (
+					<span className="text-red-500 text-sm font-mono flex items-center gap-1">
+						<AlertCircle size={14} />
+						{error}
+					</span>
+				)}
+			</Field>
+		</div>
 	)
 }
