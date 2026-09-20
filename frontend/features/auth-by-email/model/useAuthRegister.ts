ce1c19132e0dfa6dff8e9d65c9ab923d8d532a3a
krims31 <vegor405@gmail.com>
2026-09-20 15:51:51 +0300

    Fix: All also

--- frontend/features/auth-by-email/model/useAuthRegister.ts
+++ frontend/features/auth-by-email/model/useAuthRegister.ts
@@ -1,6 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
+import ApiClient from '../../../shared/api/api-client'
 import { RegisterFormValues, registerSchema } from './register.schema'
 
 export default function useAuthRegister() {
@@ -14,25 +15,17 @@ export default function useAuthRegister() {
 	})
 
 	const onSubmit = async (data: RegisterFormValues) => {
-		const response = await fetch('http://localhost:3000/auth/register', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(data)
-		})
+		try {
+			const result = await ApiClient('/auth/register', {
+				method: 'POST',
+				auth: false,
+				body: data
+			})
 
-		if (!response.ok) {
-			console.log('Register failed')
-			return
+			router.push('/auth/login')
+		} catch (error) {
+			console.log(error)
 		}
-
-		const result = await response.json()
-
-		localStorage.setItem('access_token', result.access_token)
-		localStorage.setItem('refresh_token', result.refresh_token)
-
-		router.push('/auth/login')
 	}
 
 	return { register, handleSubmit: handleSubmit(onSubmit), errors }
