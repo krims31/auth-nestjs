05212a2d4dace8b57f559dd6e3c78b8d803cbeba
krims31 <vegor405@gmail.com>
2026-09-21 14:18:11 +0300

    Fix: Added hooks useState and useEffect with logic for getProjects

--- frontend/app/kanban-board/page.tsx
+++ frontend/app/kanban-board/page.tsx
@@ -1,10 +1,35 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { getProjects } from '../../features/projects/api/projectApi'
+import { Project } from '../../features/projects/types/ProjectFromValues'
 import ProtectedRoute from '../../shared/ui/ProtectedRoute/ProtectedRoute'
 
 export default function KanbanBoard() {
+	const [project, setProject] = useState<Project[]>([])
+
+	useEffect(() => {
+		const fetchProjects = async () => {
+			try {
+				const data = await getProjects()
+				setProject(data)
+			} catch (error) {
+				console.error('Error will be loading projects:', error)
+			}
+		}
+		fetchProjects()
+	}, [])
 	return (
 		<>
 			<ProtectedRoute>
-				<h1>Kanban Board</h1>
+				<div>
+					{project.map(project => (
+						<div key={project.id}>
+							<h3>{project.title}</h3>
+							<p>{project.description}</p>
+						</div>
+					))}
+				</div>
 			</ProtectedRoute>
 		</>
 	)
