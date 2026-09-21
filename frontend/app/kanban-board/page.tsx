fc51108d7f8d9a34582e50748a2e36d39a6a1bd9
krims31 <vegor405@gmail.com>
2026-09-21 14:30:28 +0300

    feat: Added custom hook for list map Projects

--- frontend/app/kanban-board/page.tsx
+++ frontend/app/kanban-board/page.tsx
@@ -1,24 +1,10 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-import { getProjects } from '../../features/projects/api/projectApi'
-import { Project } from '../../features/projects/types/ProjectFromValues'
+import useProject from '../../features/projects/hooks/useProject'
 import ProtectedRoute from '../../shared/ui/ProtectedRoute/ProtectedRoute'
 
 export default function KanbanBoard() {
-	const [project, setProject] = useState<Project[]>([])
-
-	useEffect(() => {
-		const fetchProjects = async () => {
-			try {
-				const data = await getProjects()
-				setProject(data)
-			} catch (error) {
-				console.error('Error will be loading projects:', error)
-			}
-		}
-		fetchProjects()
-	}, [])
+	const { project } = useProject()
 	return (
 		<>
 			<ProtectedRoute>
