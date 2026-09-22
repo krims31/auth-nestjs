656d7488cdbe14809164fd9e2255267927abbb0e
krims31 <vegor405@gmail.com>
2026-09-22 08:39:40 +0300

    fix: Renamed folder on UI

---
+++ frontend/features/projects/ui/ProjectLists.tsx
@@ -0,0 +1,16 @@
+import useProject from '../hooks/useProject'
+
+export default function ProjectLists() {
+	const { project } = useProject()
+
+	return (
+		<div>
+			{project.map(project => (
+				<div key={project.id}>
+					<h3>{project.title}</h3>
+					<p>{project.description}</p>
+				</div>
+			))}
+		</div>
+	)
+}
