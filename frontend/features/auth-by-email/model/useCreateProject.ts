38efeb5a38fc91517a902899270eea52b59aabda
krims31 <vegor405@gmail.com>
2026-09-22 09:17:03 +0300

    feat: delete and route files to the new path.

--- frontend/features/auth-by-email/model/useCreateProject.ts
+++ frontend/features/auth-by-email/model/useCreateProject.ts
@@ -1,3 +1,40 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import ApiClient from '../../../shared/api/api-client'
+import { projectSchema } from '../../projects/schema/project.schema'
+import { ProjectFormValues } from '../../projects/types/ProjectFromValues'
+
 export default function useCreateProject() {
-	
+	const router = useRouter()
+
+	const { title, description, handleSubmit } = useForm<ProjectFormValues>({
+		resolver: zodResolver(projectSchema)
+	})
+
+	const [serverError, setServerError] = useState<string | null>(null)
+
+	const onSubmit = async (data: ProjectFormValues) => {
+		try {
+			await ApiClient('/projects', {
+				method: 'POST',
+				auth: false,
+				body: data
+			})
+		} catch (error) {
+			if (error instanceof Error) {
+				setServerError(error.message)
+			} else {
+				setServerError('Something went wrong')
+			}
+		}
+	}
+
+	return {
+		title,
+		description,
+		handleSubmit: handleSubmit(onSubmit),
+		serverError
+	}
 }
