4b4e354c2420e4fef474345134cdb3dcb8401824
krims31 <vegor405@gmail.com>
2026-09-22 08:23:46 +0300

    feat: Added a new file, ProjectLists.tsx, in the folder project.

--- frontend/app/kanban-board/page.tsx
+++ frontend/app/kanban-board/page.tsx
@@ -1,21 +1,13 @@
 'use client'
 
-import useProject from '../../features/projects/hooks/useProject'
+import ProjectLists from '../../features/projects/project/ProjectLists'
 import ProtectedRoute from '../../shared/ui/ProtectedRoute/ProtectedRoute'
 
 export default function KanbanBoard() {
-	const { project } = useProject()
 	return (
 		<>
 			<ProtectedRoute>
-				<div>
-					{project.map(project => (
-						<div key={project.id}>
-							<h3>{project.title}</h3>
-							<p>{project.description}</p>
-						</div>
-					))}
-				</div>
+				<ProjectLists />
 			</ProtectedRoute>
 		</>
 	)
