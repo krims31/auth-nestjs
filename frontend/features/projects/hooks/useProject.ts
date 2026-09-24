ef68cf42cd8c74d439ef5f57febd8cedcba3043b
krims31 <vegor405@gmail.com>
2026-09-24 14:48:19 +0300

    feat: Added useQuery hook with values isPending, data: project, error, and also queryKey with projects and queryFn with getProjects function.

--- frontend/features/projects/hooks/useProject.ts
+++ frontend/features/projects/hooks/useProject.ts
@@ -1,23 +1,18 @@
-import { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import { getProjects } from '../api/projectApi'
-import { Project } from '../types/ProjectFromValues'
-
 export default function useProject() {
-	const [project, setProject] = useState<Project[]>([])
-
-	useEffect(() => {
-		const fetchProjects = async () => {
-			try {
-				const data = await getProjects()
-				setProject(data)
-			} catch (error) {
-				console.error('Error will be loading projects:', error)
-			}
-		}
-		fetchProjects()
-	}, [])
+	const {
+		isPending,
+		data: project,
+		error
+	} = useQuery({
+		queryKey: ['projects'],
+		queryFn: getProjects
+	})
 
 	return {
-		project
+		project,
+		isPending,
+		error
 	}
 }
