e8f515842040240644e80d89723beabf7586e217
krims31 <vegor405@gmail.com>
2026-09-24 14:46:46 +0300

    feat: Added isPending and check on Error with message

--- frontend/features/projects/ui/ProjectLists.tsx
+++ frontend/features/projects/ui/ProjectLists.tsx
@@ -1,11 +1,19 @@
 import useProject from '../hooks/useProject'
 
 export default function ProjectLists() {
-	const { project } = useProject()
+	const { project, isPending, error } = useProject()
+
+	if (isPending) {
+		return <div>Loading...</div>
+	}
+
+	if (error instanceof Error) {
+		return <span>{error.message}</span>
+	}
 
 	return (
 		<div>
-			{project.map(project => (
+			{project?.map(project => (
 				<div key={project.id}>
 					<h3>{project.title}</h3>
 					<p>{project.description}</p>
