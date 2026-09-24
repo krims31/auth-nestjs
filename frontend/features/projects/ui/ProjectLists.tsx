cad6fde5a17c16e2a52904e28b957fdfff6de922
krims31 <vegor405@gmail.com>
2026-09-24 14:49:52 +0300

    feat: Added commentary

--- frontend/features/projects/ui/ProjectLists.tsx
+++ frontend/features/projects/ui/ProjectLists.tsx
@@ -3,14 +3,17 @@ import useProject from '../hooks/useProject'
 export default function ProjectLists() {
 	const { project, isPending, error } = useProject()
 
+	// Pending loading.
 	if (isPending) {
 		return <div>Loading...</div>
 	}
 
+	// Check error message and return error message.
 	if (error instanceof Error) {
 		return <span>{error.message}</span>
 	}
 
+	// Return lists of projects.
 	return (
 		<div>
 			{project?.map(project => (
