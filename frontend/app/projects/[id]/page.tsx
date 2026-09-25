949c9df27a4efbb683d67e0672d7af20d1379411
krims31 <vegor405@gmail.com>
2026-09-25 10:50:56 +0300

    feat: Added projectId with hook useTask and add lists

--- frontend/app/projects/[id]/page.tsx
+++ frontend/app/projects/[id]/page.tsx
@@ -1,11 +1,28 @@
 import { useParams } from 'next/navigation'
+import useTask from '../../../features/tasks/hooks/useTask'
 
 export default function ProjectId() {
 	const params = useParams()
 
+	const { tasks, isPending, error } = useTask(params.id as string)
+
+	if (isPending) {
+		return <span>Loading...</span>
+	}
+
+	if (error instanceof Error) {
+		return <span>{error.message}</span>
+	}
+
 	return (
 		<div>
 			<h1>{params.id}</h1>
+			{tasks?.map(task => (
+				<div key={task.id}>
+					<p>{task.title}</p>
+					<p>{task.status}</p>
+				</div>
+			))}
 		</div>
 	)
 }
