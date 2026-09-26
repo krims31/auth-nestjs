9092a66c7a3d9ca581acfe206a638ff1db2a478b
krims31 <vegor405@gmail.com>
2026-09-26 09:21:17 +0300

    feat: Added file CreateTaskForm with the projectId

--- frontend/features/tasks/ui/ProjectId.tsx
+++ frontend/features/tasks/ui/ProjectId.tsx
@@ -1,29 +1,11 @@
-import { AlertCircle } from 'lucide-react'
-import InputProject from '../../../shared/ui/input-project/inputProject'
-import useCreateProject from '../../projects/hooks/useCreateProject'
+import { useParams } from "next/navigation"
+import CreateTaskForm from "./CreateTaskForm"
 
-export default function CreateProjectForm() {
-	const { register, errors, serverError, handleSubmit } = useCreateProject()
-
-	return (
-		<>
-			<form onSubmit={handleSubmit}>
-				<InputProject
-					registration={register('title')}
-					label="Title"
-					placeholder="Title"
-					error={errors.title?.message}
-				/>
-				{serverError && (
-					<span className="text-red-500 text-sm font-mono flex items-center gap-1">
-						<AlertCircle size={14} />
-						{serverError}
-					</span>
-				)}
-				<button type="submit">
-					<span className="text-sm font-mono">Submit</span>
-				</button>
-			</form>
-		</>
-	)
+export default function ProjectId() {
+  const params = useParams()
+  return (
+    <>
+      <CreateTaskForm projectId={params.id as string}></CreateTaskForm>
+    </>
+  )
 }
