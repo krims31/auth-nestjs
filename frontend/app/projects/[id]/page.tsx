964e1d23c2490a373def42620b5a9f9a00623530
krims31 <vegor405@gmail.com>
2026-09-26 09:58:26 +0300

    feat: Added use client in the file

--- frontend/app/projects/[id]/page.tsx
+++ frontend/app/projects/[id]/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useParams } from "next/navigation"
 import useTask from "../../../features/tasks/hooks/useTask"
 import CreateTaskForm from "../../../features/tasks/ui/CreateTaskForm"
