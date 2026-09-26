5a587fc9a4302162c0dbd19aa193f77c7146adbb
krims31 <vegor405@gmail.com>
2026-09-26 09:54:40 +0300

    fix: fix logic

--- frontend/app/projects/[id]/page.tsx
+++ frontend/app/projects/[id]/page.tsx
@@ -1,32 +1,27 @@
-'use client'
-
-import { useParams } from 'next/navigation'
-import useTask from '../../../features/tasks/hooks/useTask'
+import { useParams } from "next/navigation"
+import useTask from "../../../features/tasks/hooks/useTask"
+import CreateTaskForm from "../../../features/tasks/ui/CreateTaskForm"
 
 export default function ProjectId() {
-	const params = useParams()
+  const params = useParams()
+  const projectId = params.id as string
 
-	const { tasks, isPending, error } = useTask(params.id as string)
+  const {tasks, isPending, error} = useTask(projectId)
 
-	if (isPending) {
-		return (
-			<span className="text-2xl flex items-center h-screen">Loading...</span>
-		)
-	}
+  return (
+    <>
+      <h1>{projectId}</h1>
+      <CreateTaskForm projectId={projectId}></CreateTaskForm>
 
-	if (error instanceof Error) {
-		return <span>{error.message}</span>
-	}
+      {isPending && <span>Loading...</span>}
+      {error instanceof Error && <span>{error.message}</span>}
 
-	return (
-		<div>
-			<h1>{params.id}</h1>
-			{tasks?.map(task => (
-				<div key={task.id}>
-					<p>{task.title}</p>
-					<p>{task.status}</p>
-				</div>
-			))}
-		</div>
-	)
+      {tasks?.map(task => (
+        <li key={task.id}>
+          <p>{task.title}</p>
+          <p>{task.status}</p>
+        </li>
+      ))}
+    </>
+  )
 }
