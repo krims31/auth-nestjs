a4fe9e80dd459c79c935b349a721b2246d235ce1
krims31 <vegor405@gmail.com>
2026-09-26 09:30:51 +0300

    feat: change all file on the normal logic

--- frontend/features/tasks/ui/ProjectId.tsx
+++ frontend/features/tasks/ui/ProjectId.tsx
@@ -1,11 +1,27 @@
 import { useParams } from "next/navigation"
 import CreateTaskForm from "./CreateTaskForm"
+import useTask from "../hooks/useTask"
 
 export default function ProjectId() {
   const params = useParams()
+  const projectId = params.id as string
+
+  const {tasks, isPending, error} = useTask(projectId)
+
   return (
     <>
-      <CreateTaskForm projectId={params.id as string}></CreateTaskForm>
+      <h1>{projectId}</h1>
+      <CreateTaskForm projectId={projectId}></CreateTaskForm>
+
+      {isPending && <span>Loading...</span>}
+      {error instanceof Error && <span>{error.message}</span>}
+
+      {tasks?.map(task => (
+        <li key={task.id}>
+          <p>{task.title}</p>
+          <p>{task.status}</p>
+        </li>
+      ))}
     </>
   )
 }
